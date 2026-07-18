90906f4bd2be64ac2deb25bd7725222b0c240dd8
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-18 12:23:15 +0600

    added crud read and delete method

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Users from './components/Users'
 
 function App() {
 
-
+const userPromise = fetch("http://localhost:3000/users").then(res=>res.json())
   return (
     <>
 <h1>simple crud operation</h1>
-<Users></Users>
+<Suspense fallback={<p>loading...</p>}>
+<Users userPromise = {userPromise}></Users>
+</Suspense>
+
     </>
   )
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { use, useState }  from 'react';
 
-const Users = () => {
+const Users = ({userPromise}) => {
+    const initialUser =  use(userPromise);
+    const [users, setUsers] = useState(initialUser)
+    console.log(initialUser);
+    
+
     const handleForm = e =>{
         e.preventDefault();
         const name = e.target.name.value;
          const email = e.target.email.value;
-         const users = {name, email};
-         console.log(users);
+         const newUser = {name, email};
+         console.log(newUser);
          fetch('http://localhost:3000/users', {
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
             },
-            body:JSON.stringify(users)
+            body:JSON.stringify(newUser)
          })
          .then(res=>res.json())
          .then(data=>{
             console.log('data after creating in db user :',data);
-            
-         })
-         
+                  if(data.insertedId){
+                    newUser._id = data.initialUser;
+                    const newUsers = [...users, newUser];
+                    setUsers(newUsers)
+                alert('user added successfully');
+                e.target.reset()
+            }
+         })    
     }
+             const handleUserDelete = (id) =>{
+            console.log('user delete from database', id);
+            fetch(`http://localhost:3000/users/${id}`,{
+                method:"DELETE"
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.deletedCount){
+                    const remainingUsers = users.filter((user)=>user._id !== id);
+                    setUsers(remainingUsers)
+                     console.log("after user delete" , data);  
+                }
+                
+            })
+            
+         }
     return (
         <div>
+            <h2>user : {initialUser.length}</h2>
             <form onSubmit={handleForm}>
                 <input name='name' type="text" placeholder='Name' /> <br />
                 <input type="email" name="email" placeholder='Email' /> <br />
                 <input type="submit" value="Add user" />
             </form>
+            {
+              users.map((user)=><p key={user._id}>{user.name} = {user.email} <button onClick={()=>handleUserDelete(user._id)} className='btn'> X</button></p>)
+            }
             
         </div>
     );
